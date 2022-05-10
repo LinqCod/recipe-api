f66f8ee84fcf8b8637d6223d26f590c0f393a5da
Maxim <linqcod@yandex.ru>
2022-05-11 00:10:45 +0300

    recipe data model added

---
+++ model.go
@@ -0,0 +1,11 @@
+package recipe_api
+
+import "time"
+
+type Recipe struct {
+	Name         string    `json:"name"`
+	Tags         []string  `json:"tags"`
+	Ingredients  []string  `json:"ingredients"`
+	Instructions []string  `json:"instructions"`
+	PublishedAt  time.Time `json:"publishedAt"`
+}
